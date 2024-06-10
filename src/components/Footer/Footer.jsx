cb0db13d8000a62bb789593eabb0376ad9d8d786
benjamin <benjaminmusic98@gmail.com>
2024-06-10 18:34:11 +0200

    neki

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
@@ -19,27 +20,25 @@ const Footer = () => {
                     <div className="footer__content--item">
                         <h3 className="footer__title">Projects</h3>
                         <ul className="footer__links">
-                            <li><a href = "#">About</a></li>
-                            <li><a href = "#">Services</a></li>
-                            <li><a href = "#">Career</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="footer__content--item">
-                        <h3 className="footer__title">News</h3>
-                        <ul className="footer__links">
-                            <li><a href = "#">Events</a></li>
-                            <li><a href = "#">Contact</a></li>
-                            <li><a href = "#">Legals</a></li>
+                            <li><Link to='/'  className="nav__link text__upper fw__6
+                                text__light ">Domov</Link></li>
+                            <li><Link to='/about' className="nav__link text__upper fw__6
+                                text__light ">Onas</Link></li>
+                            <li><Link to='/gallery' className="nav__link text__upper fw__6
+                                text__light ">Galerija</Link></li>
+                            <li><Link to='/buy' className="nav__link text__upper fw__6
+                                text__light ">Nakup</Link></li>
+                            <li><Link to='/trip' className="nav__link text__upper fw__6
+          text__light ">Izleti</Link></li>
                         </ul>
                     </div>
 
                     <div className="footer__content--item">
                         <h3 className="footer__title">Social LInks</h3>
                         <ul className="footer__links">
-                            <li><a href = "#">Facebook</a></li>
-                            <li><a href = "#">Twitter</a></li>
-                            <li><a href = "#">Instagram</a></li>
+                            <li><a href = "/">Facebook</a></li>
+                            <li><a href = "/">Twitter</a></li>
+                            <li><a href = "/">Instagram</a></li>
                         </ul>
                     </div>
                 </div>
