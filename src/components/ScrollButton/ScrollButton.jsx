fac5f161514e2aa27aa725cb323b1c094b801a54
Benjamin Mušić <Benjamin.Music@comtradegaming.com>
2024-07-01 18:54:09 +0200

    nojira
    fix for deploy

--- src/components/ScrollButton/ScrollButton.jsx
+++ src/components/ScrollButton/ScrollButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ScrollButton(){
   
   return <div className="App">
-    <a className="btn btn__gold" href="#">
+    <a className="btn btn__gold" href="/">
     Do vrha
     </a>
   </div>
